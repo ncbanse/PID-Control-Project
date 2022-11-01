97cf7d32ec2b6bc528a9ec2af8fc30fcb8537253
ncbanse <ncbanse@icloud.com>
2022-11-01 13:39:49 -0700

    angle change detect

--- TEST proportional control/TEST proportional control.ino
+++ TEST proportional control/TEST proportional control.ino
@@ -1,9 +1,17 @@
 void setup() {
   // put your setup code here, to run once:
-
+  double angle = 0;
+  int encodeRes = 2^12 * 4;
+  double angleIn = 90;
+  double encodeFreq = 5;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  bool digitalRead(2) = encode1;
+  bool digitalRead(3) = encode2;
+  delay(5);
+  if (encode1 != digitalRead(2) || encode2 != digitalRead(3)) {
+    angle = angle + 360/encodeRes;
+  }
 
 }
