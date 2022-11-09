cf67ea87a9dbf4fef60c8686f12a27e756ae1def
ncbanse <ncbanse@icloud.com>
2022-11-09 12:52:31 -0800

    Changed UART Test Name

--- uartTest/uartTest.ino
+++ uartTest/uartTest.ino
@@ -11,7 +11,7 @@
 VescUart UART;
 
 float current = 5.0; /** The current in amps */
-float RPM = 600;
+float duty = 0.2;
 
 void setup() {
   Serial.begin(9600);
@@ -26,9 +26,17 @@ void setup() {
 void loop() {
   
   /** Call the function setCurrent() to set the motor current */
-  UART.setCurrent(current);
-  dellay(50);
-  current -= .1;
-  // UART.setBrakeCurrent(current);
+  /**while (current >= 0) {
+    UART.setCurrent(current);
+    dellay(50);
+    current -= .1;*/
+  }
   
+  // UART.setBrakeCurrent(current);
+  while (duty >= 0) {
+    UART.setDuty(duty);
+    dellay(50);
+    duty -= 0.01;
+  }
+
 }
